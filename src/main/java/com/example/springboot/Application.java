c8237e82882ac02b5e52445635ee09f20d2089d2
kpayins147 <foxliontiger@gmail.com>
2021-01-29 17:41:42 +0900

    [SR-2021-01][홍길동][1]샘플코드작성 - 이슈: git-flow 정책 시연 및 테스트 - 원인: - 수정: 샘플코드작성

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,28 @@
+package com.example.springboot;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@Slf4j
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            log.info("Let's inspect the beans provided by Spring Boot:");
+
+        };
+    }
+
+}
